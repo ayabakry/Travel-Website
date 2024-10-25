30b633481ea4b063c15adafca653f4f4049461c6
ayabakry <ayatbakry@yahoo.com>
2024-10-25 15:34:00 +0300

    add card design ++

--- src/Components/Travelcard.jsx
+++ src/Components/Travelcard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import imgcard from "../Imgs/travelcard.png"
 
 
@@ -9,22 +10,22 @@ function Travelcard() {
 
         <div className="flex justify-end px-8 py-8">
             <div className="max-w-sm bg-white rounded-3xl shadow ">
-                <a href="#">
+                <Link to="">
                     <img className="rounded-tl-3xl rounded-tr-3xl w-full" src={imgcard} alt="no img" />
-                </a>
+                    </Link>
                 <div className="p-5">
-                    <a href="#">
+                    <Link to="#">
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-primaryTitle text-right">شرم الشيخ</h5>
-                    </a>
+                    </Link>
                     <p className="mb-3 font-normal text-right text-bodycolor">بدأت المغامرة بالطيران إلى هذا الجمال الساحر المطل على البحر الأحمر</p>
                     <p className="mb-3 font-normal text-gray-700 text-right">EGP <span className="text-price text-lg font-bold">125</span></p>
                     <button className="inline-flex items-center px-6 py-1 text-sm text-center text-white bg-secondarybutton rounded-3xl ">
-                        <a href="#" >
+                        <Link to="#" >
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M9.57031 5.93018L3.50031 12.0002L9.57031 18.0702" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
                                 <path d="M20.5 12H3.67" stroke="white" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
                             </svg>
-                        </a>
+                        </Link>
                     </button>
                 </div>
             </div>
